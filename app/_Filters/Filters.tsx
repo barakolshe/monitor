1ee2d0d786e6c1db7a36640decf6de3157f42e82
barakolshe <barakolshe@gmail.com>
2023-10-16 14:20:46 +0000

    added config file

--- app/_Filters/Filters.tsx
+++ app/_Filters/Filters.tsx
@@ -7,6 +7,7 @@ import { FilterContext } from "@/context/FilterContext";
 import { RocketsFilter } from "@/types/rockets/RocketsFilter.interface";
 import cloneDeep from "lodash.clonedeep";
 import { cn } from "@/lib/tailwindUtils";
+import { PREDEFINED_FITLERS } from "@/configs/mainConfig";
 
 interface FiltersProps {}
 
@@ -14,27 +15,6 @@ interface PredifinedFilter {
   title: string;
   locations: string[];
 }
-
-const PREDEFINED_FITLERS = [
-  {
-    title: "Jersulem",
-    locations: [
-      "עמינדב",
-      "אורה",
-      "ירושלים - מרכז",
-      "ירושלים - מערב",
-      "ירושלים - דרום",
-      "ירושלים - מזרח",
-      "פנימיית עין כרם",
-      "אבן ספיר",
-      "מבשרת ציון",
-      "בית זית",
-      "ירושלים - צפון",
-      "מוצא עילית",
-      "גבעת זאב",
-    ],
-  },
-];
 
 const Filters: FunctionComponent<FiltersProps> = () => {
   const { filter: filter, setFilter: setFilter } =
